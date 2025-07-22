0f43786b3989d6fd4bbdcfbd690844a03ba45fc5
JaeYoungJung <hanol98@naver.com>
2025-07-22 12:19:37 +0900

    init: 프로젝트 생성

---
+++ src/main/java/org/fadak/selp/selpbackend/SelpBackendApplication.java
@@ -0,0 +1,14 @@
+package org.fadak.selp.selpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelpBackendApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SelpBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fadak/selp/selpbackend/SelpBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package org.fadak.selp.selpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
