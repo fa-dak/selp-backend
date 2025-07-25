8370b4715f6481800db91170b61823bf5fcb5e17
sangzun-han <hsjun1996@naver.com>
2025-07-25 22:49:14 +0900

    feat : FCM토큰 등록 및 업데이트

---
+++ src/main/java/org/fadak/selp/selpbackend/application/service/FcmTokenService.java
@@ -0,0 +1,7 @@
+package org.fadak.selp.selpbackend.application.service;
+
+import org.fadak.selp.selpbackend.domain.dto.request.FcmTokenRegisterRequestDto;
+
+public interface FcmTokenService {
+    void saveOrUpdateToken(Long memberId, FcmTokenRegisterRequestDto request);
+}
