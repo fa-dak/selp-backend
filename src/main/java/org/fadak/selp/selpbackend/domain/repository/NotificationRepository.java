30d577720fb1ccf8e566ee06730d1a993d8c0e8d
sangzun-han <hsjun1996@naver.com>
2025-07-27 02:08:41 +0900

    feat: 알림 등록을 위한 NotificationRepository

--- src/main/java/org/fadak/selp/selpbackend/domain/repository/NotificationRepository.java
+++ src/main/java/org/fadak/selp/selpbackend/domain/repository/NotificationRepository.java
@@ -4,7 +4,11 @@ import org.fadak.selp.selpbackend.domain.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findBySendDateAndIsSent(LocalDate sendDate, Boolean isSent);
 
 }
