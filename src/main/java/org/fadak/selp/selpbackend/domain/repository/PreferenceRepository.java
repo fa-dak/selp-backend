d5a47ce38dc742818059998aa388aedb95108f9f
pch8349 <chan449@naver.com>
2025-07-29 17:35:12 +0900

    feat : 주변인 정보 수정 시 카테고리 삭제

--- src/main/java/org/fadak/selp/selpbackend/domain/repository/PreferenceRepository.java
+++ src/main/java/org/fadak/selp/selpbackend/domain/repository/PreferenceRepository.java
@@ -3,9 +3,13 @@ package org.fadak.selp.selpbackend.domain.repository;
 import org.fadak.selp.selpbackend.domain.entity.Preference;
 import org.fadak.selp.selpbackend.domain.entity.ReceiverInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.Optional;
 
 public interface PreferenceRepository extends JpaRepository<Preference, Long> {
     Optional<Preference> findByReceiverInfo(ReceiverInfo receiverInfo);
+
+    @Modifying
+    void deleteAllByReceiverInfoId(Long receiverInfoId);
 }
