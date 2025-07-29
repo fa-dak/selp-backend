52dfd6b929f6b0cdb8986118673245a4e33825b6
JaeYoungJung <hanol98@naver.com>
2025-07-29 15:03:35 +0900

    comment: 클라이언트 요청 시, 인증 정보 로그 출력

--- src/main/java/org/fadak/selp/selpbackend/application/security/JwtAuthenticationFilter.java
+++ src/main/java/org/fadak/selp/selpbackend/application/security/JwtAuthenticationFilter.java
@@ -4,27 +4,40 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.fadak.selp.selpbackend.domain.auth.UserPrincipal;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-
+@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+
         String token = resolveToken(request);
 
         if (token != null) {
-            try{
+            try {
                 jwtTokenProvider.validateToken(token);
                 Authentication authentication = jwtTokenProvider.getAuthentication(token);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
+
+                Object principal = authentication.getPrincipal();
+                if (principal instanceof UserPrincipal userPrincipal) {
+                    log.info("[Authenticated] memberId={} | email: {}", userPrincipal.getId(),
+                        userPrincipal.getUsername());
+                } else {
+                    log.warn("Unexpected principal type: {}", principal.getClass());
+                }
             } catch (IllegalArgumentException e) {
                 SecurityContextHolder.clearContext();
             }
@@ -33,6 +46,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     private String resolveToken(HttpServletRequest request) {
+
         String bearerToken = request.getHeader("Authorization");
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7);
