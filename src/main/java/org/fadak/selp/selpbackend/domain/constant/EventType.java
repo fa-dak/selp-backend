f63f759b179adc4ac29bd410ea4d3b3190c8e9b1
JaeYoungJung <hanol98@naver.com>
2025-07-29 14:31:30 +0900

    event type 추가

--- src/main/java/org/fadak/selp/selpbackend/domain/constant/EventType.java
+++ src/main/java/org/fadak/selp/selpbackend/domain/constant/EventType.java
@@ -8,8 +8,11 @@ import lombok.Getter;
 public enum EventType {
 
     BIRTHDAY("생일"),
+    MEETING("미팅"),
+    VACATION("방학"),
     ANNIVERSARY("기념일"),
     WEDDING_ANNIVERSARY("결혼 기념일"),
+    GRADUATION("졸업"),
     HOLIDAY("휴일"),
     ETC("기타");
 
