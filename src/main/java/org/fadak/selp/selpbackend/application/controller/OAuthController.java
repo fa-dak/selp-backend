3b33e4efa5e264d9d7ec9099fd82b64f77fe7e9e
JaeYoungJung <hanol98@naver.com>
2025-07-29 09:51:41 +0900

    fix: 로그인 api만 user principal 갖져오지 않도록 수정

--- src/main/java/org/fadak/selp/selpbackend/application/controller/OAuthController.java
+++ src/main/java/org/fadak/selp/selpbackend/application/controller/OAuthController.java
@@ -3,11 +3,9 @@ package org.fadak.selp.selpbackend.application.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.fadak.selp.selpbackend.application.service.OAuthService;
-import org.fadak.selp.selpbackend.domain.auth.UserPrincipal;
 import org.fadak.selp.selpbackend.domain.dto.request.KakaoLoginRequestDto;
 import org.fadak.selp.selpbackend.domain.dto.response.LoginResponseDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,11 +21,9 @@ public class OAuthController {
 
     @PostMapping("/kakao")
     public ResponseEntity<LoginResponseDto> loginWithKakao(
-        @AuthenticationPrincipal UserPrincipal userPrincipal,
         @RequestBody KakaoLoginRequestDto request
     ) {
 
-        long loginMemberId = userPrincipal.getId();
         log.info("카카오 로그인 요청이 들어왔습니다. 인가 코드: {}", request.getAccessToken());
         LoginResponseDto responseDto = oAuthService.loginWithKakao(request);
         return ResponseEntity.ok(responseDto);
