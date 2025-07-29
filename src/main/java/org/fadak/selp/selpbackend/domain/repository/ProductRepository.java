634dc7841277e524d3bfaacfc85670285b9c3adb
pch8349 <chan449@naver.com>
2025-07-29 17:36:16 +0900

    Merge Conflict Resolve

--- src/main/java/org/fadak/selp/selpbackend/domain/repository/ProductRepository.java
+++ src/main/java/org/fadak/selp/selpbackend/domain/repository/ProductRepository.java
@@ -1,11 +1,12 @@
 package org.fadak.selp.selpbackend.domain.repository;
 
-import java.util.List;
 import org.fadak.selp.selpbackend.domain.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
