0575e7fe64fa9b8aa538dfb99fb9818e775eeff4
JaeYoungJung <hanol98@naver.com>
2025-07-30 16:04:43 +0900

    fix: Jackson 관련 implementation 추가 및 restTemplate 설정 수정

--- src/main/java/org/fadak/selp/selpbackend/configuration/AppConfig.java
+++ src/main/java/org/fadak/selp/selpbackend/configuration/AppConfig.java
@@ -1,17 +1,16 @@
 package org.fadak.selp.selpbackend.configuration;
 
-import java.util.List;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
 
-        return new RestTemplate(List.of(new MappingJackson2HttpMessageConverter()));
+        return builder.build(); // Spring Boot가 Jackson 포함한 기본 message converters 자동 구성
     }
 }
