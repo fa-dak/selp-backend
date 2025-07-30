eaa99aeb88fb81b2c8feb93f618ab6f0631e8e42
JaeYoungJung <hanol98@naver.com>
2025-07-30 15:45:47 +0900

    fix: 꾸러미 생성 시 PayStatus 세팅

--- src/main/java/org/fadak/selp/selpbackend/application/service/GiftBundleFacadeServiceImpl.java
+++ src/main/java/org/fadak/selp/selpbackend/application/service/GiftBundleFacadeServiceImpl.java
@@ -1,22 +1,36 @@
 package org.fadak.selp.selpbackend.application.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.fadak.selp.selpbackend.application.util.OpenAiBuilderUtil;
+import org.fadak.selp.selpbackend.domain.constant.PayStatus;
 import org.fadak.selp.selpbackend.domain.dto.request.GiftBundleRecommendRequestDto;
 import org.fadak.selp.selpbackend.domain.dto.request.GiftBundleSaveFromCalendarRequestDto;
 import org.fadak.selp.selpbackend.domain.dto.request.GiftBundleSaveRequestDto;
 import org.fadak.selp.selpbackend.domain.dto.request.GiftRecommendAgainRequestDto;
 import org.fadak.selp.selpbackend.domain.dto.response.GiftBundleItemResponseDto;
-import org.fadak.selp.selpbackend.domain.entity.*;
-import org.fadak.selp.selpbackend.domain.repository.*;
+import org.fadak.selp.selpbackend.domain.entity.Event;
+import org.fadak.selp.selpbackend.domain.entity.GiftBundle;
+import org.fadak.selp.selpbackend.domain.entity.GiftBundleItem;
+import org.fadak.selp.selpbackend.domain.entity.Member;
+import org.fadak.selp.selpbackend.domain.entity.Preference;
+import org.fadak.selp.selpbackend.domain.entity.Product;
+import org.fadak.selp.selpbackend.domain.entity.ProductCategory;
+import org.fadak.selp.selpbackend.domain.entity.ReceiverInfo;
+import org.fadak.selp.selpbackend.domain.repository.EventRepository;
+import org.fadak.selp.selpbackend.domain.repository.GiftBundleItemRepository;
+import org.fadak.selp.selpbackend.domain.repository.GiftBundleRepository;
+import org.fadak.selp.selpbackend.domain.repository.MemberRepository;
+import org.fadak.selp.selpbackend.domain.repository.PreferenceRepository;
+import org.fadak.selp.selpbackend.domain.repository.ProductCategoryRepository;
+import org.fadak.selp.selpbackend.domain.repository.ProductRepository;
+import org.fadak.selp.selpbackend.domain.repository.ReceiverInfoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -36,10 +50,12 @@ public class GiftBundleFacadeServiceImpl implements GiftBundleFacadeService {
     private final PreferenceRepository preferenceRepository;
 
     @Override
-    public List<GiftBundleItemResponseDto> recommendGiftBundle(GiftBundleRecommendRequestDto requestDto) {
+    public List<GiftBundleItemResponseDto> recommendGiftBundle(
+        GiftBundleRecommendRequestDto requestDto) {
 
         // 사용자의 입력 데이터를 바탕으로 카테고리별 금액을 산정
-        Map<String, Integer> categoryBudgetMap = categoryInferenceService.distributeBudget(requestDto);
+        Map<String, Integer> categoryBudgetMap = categoryInferenceService.distributeBudget(
+            requestDto);
 
         List<GiftBundleItemResponseDto> result = new ArrayList<>();
 
@@ -47,14 +63,16 @@ public class GiftBundleFacadeServiceImpl implements GiftBundleFacadeService {
             String category = entry.getKey();
             Integer budget = entry.getValue();
 
-            String textForEmbedding = OpenAiBuilderUtil.buildEmbeddingPrompt(requestDto, category, budget);
+            String textForEmbedding = OpenAiBuilderUtil.buildEmbeddingPrompt(requestDto, category,
+                budget);
 
-            Map<String, Object> searchResult = embeddingSearcher.searchMostRelevant(textForEmbedding, category, budget);
+            Map<String, Object> searchResult = embeddingSearcher.searchMostRelevant(
+                textForEmbedding, category, budget);
 
             Long productId = Long.valueOf((String) searchResult.get("product_id"));
 
             Product product = productRepository.findById(productId)
-                    .orElseThrow(IllegalArgumentException::new);
+                .orElseThrow(IllegalArgumentException::new);
 
             result.add(GiftBundleItemResponseDto.from(product));
         }
@@ -63,11 +81,16 @@ public class GiftBundleFacadeServiceImpl implements GiftBundleFacadeService {
     }
 
     @Override
-    public GiftBundleItemResponseDto recommendGiftBundleItem(GiftRecommendAgainRequestDto requestDto) {
-        GiftBundleRecommendRequestDto dto = requestDto.toGiftBundleRecommendRequestDto();
-        String textForEmbedding = OpenAiBuilderUtil.buildEmbeddingPrompt(dto, requestDto.getCategory(), requestDto.getPrice());
+    public GiftBundleItemResponseDto recommendGiftBundleItem(
+        GiftRecommendAgainRequestDto requestDto) {
 
-        Map<String, Object> searchResult = embeddingSearcher.recommendSimilarProduct(textForEmbedding, requestDto.getCategory(), requestDto.getPrice(), requestDto.getProductId());
+        GiftBundleRecommendRequestDto dto = requestDto.toGiftBundleRecommendRequestDto();
+        String textForEmbedding = OpenAiBuilderUtil.buildEmbeddingPrompt(dto,
+            requestDto.getCategory(), requestDto.getPrice());
+
+        Map<String, Object> searchResult = embeddingSearcher.recommendSimilarProduct(
+            textForEmbedding, requestDto.getCategory(), requestDto.getPrice(),
+            requestDto.getProductId());
 
         if (searchResult == null) {
             throw new IllegalArgumentException("상품 재추천 오류");
@@ -76,7 +99,7 @@ public class GiftBundleFacadeServiceImpl implements GiftBundleFacadeService {
         Long productId = Long.valueOf((String) searchResult.get("product_id"));
 
         Product product = productRepository.findById(productId)
-                .orElseThrow(IllegalArgumentException::new);
+            .orElseThrow(IllegalArgumentException::new);
 
         return GiftBundleItemResponseDto.from(product);
     }
@@ -86,7 +109,7 @@ public class GiftBundleFacadeServiceImpl implements GiftBundleFacadeService {
     public void registerGiftBundle(GiftBundleSaveRequestDto requestDto, Long memberId) {
 
         Member member = memberRepository.findById(memberId)
-                .orElseThrow(IllegalArgumentException::new);
+            .orElseThrow(IllegalArgumentException::new);
 
         ReceiverInfo receiverInfo = requestDto.toReceiverInfo(member);
 
@@ -99,71 +122,74 @@ public class GiftBundleFacadeServiceImpl implements GiftBundleFacadeService {
         // 주변인 선호 저장
         List<String> categories = requestDto.getCategories();
 
-        List<ProductCategory> productCategories = productCategoryRepository.findByNameIn(categories);
+        List<ProductCategory> productCategories = productCategoryRepository.findByNameIn(
+            categories);
 
         List<Preference> preferences = productCategories.stream()
-                .map(category -> Preference.builder()
-                        .receiverInfo(receiverInfoEntity)       // 이미 저장된 ReceiverInfo
-                        .category(category)
-                        .build())
-                .toList();
+            .map(category -> Preference.builder()
+                .receiverInfo(receiverInfoEntity)       // 이미 저장된 ReceiverInfo
+                .category(category)
+                .build())
+            .toList();
 
         preferenceRepository.saveAll(preferences);
 
-
         // 꾸러미 저장
         GiftBundle giftBundle = GiftBundle.builder()
-                .event(eventEntity)
-                .member(member)
-                .build();
+            .event(eventEntity)
+            .member(member)
+            .currentPayStatus(PayStatus.NOT_STARTED)
+            .build();
 
         GiftBundle giftBundleEntity = giftBundleRepository.save(giftBundle);
 
-
         // 선물 내역 저장
         List<GiftBundleItem> itemEntities = requestDto.getGiftIds().stream()
-                .map(productId -> {
-                    Product product = productRepository.findById(productId)
-                            .orElseThrow(() -> new IllegalArgumentException("상품 없음: " + productId));
+            .map(productId -> {
+                Product product = productRepository.findById(productId)
+                    .orElseThrow(() -> new IllegalArgumentException("상품 없음: " + productId));
 
-                    return GiftBundleItem.builder()
-                            .giftBundle(giftBundleEntity)
-                            .product(product)
-                            .build();
-                })
-                .collect(Collectors.toList());
+                return GiftBundleItem.builder()
+                    .giftBundle(giftBundleEntity)
+                    .product(product)
+                    .build();
+            })
+            .collect(Collectors.toList());
 
         giftBundleItemRepository.saveAll(itemEntities);
     }
 
     @Override
     @Transactional
-    public void registerGiftBundleFromCalendar(GiftBundleSaveFromCalendarRequestDto requestDto, Long memberId) {
+    public void registerGiftBundleFromCalendar(GiftBundleSaveFromCalendarRequestDto requestDto,
+        Long memberId) {
+
         Member member = memberRepository.findById(memberId)
-                .orElseThrow(IllegalArgumentException::new);
+            .orElseThrow(IllegalArgumentException::new);
 
         Event event = eventRepository.findById(requestDto.getEventId())
-                .orElseThrow(IllegalArgumentException::new);
+            .orElseThrow(IllegalArgumentException::new);
 
         GiftBundle giftBundle = GiftBundle.builder()
-                .event(event)
-                .member(member)
-                .build();
+            .event(event)
+            .member(member)
+            .currentPayStatus(PayStatus.NOT_STARTED)
+            .build();
 
         GiftBundle giftBundleEntity = giftBundleRepository.save(giftBundle);
 
         // 선물 내역 저장
         List<GiftBundleItem> itemEntities = requestDto.getGiftIds().stream()
-                .map(productId -> {
-                    Product product = productRepository.findById(productId)
-                            .orElseThrow(() -> new IllegalArgumentException("상품 없음: " + productId));
+            .map(productId -> {
+                Product product = productRepository.findById(productId)
+                    .orElseThrow(() -> new IllegalArgumentException("상품 없음: " + productId));
 
-                    return GiftBundleItem.builder()
-                            .giftBundle(giftBundleEntity)
-                            .product(product)
-                            .build();
-                })
-                .collect(Collectors.toList());
+                return GiftBundleItem.builder()
+                    .giftBundle(giftBundleEntity)
+                    .product(product)
+                    .build();
+            })
+            .collect(Collectors.toList());
 
         giftBundleItemRepository.saveAll(itemEntities);
     }
